7e996d420e68c001606e738b32c0a8963181bd09
KyleAstudillo <kyle.astudillo@yahoo.com>
2018-10-24 14:17:52 -0700

    Log4J Working!!!

--- src/hangman/Hangman.java
+++ src/hangman/Hangman.java
@@ -6,18 +6,27 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 
 public class Hangman extends Application {
 
+	private static final Logger logger = LogManager.getLogger("Hangman");
+
 	@Override
 	public void start(final Stage primaryStage) throws IOException {
+		logger.info("Starting Application");
 		final Game game = new Game();
+        logger.info("Loading Hangman.fxml");
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("Hangman.fxml"));
+        logger.info("Setting Controller");
 		loader.setController(new GameController(game));
 		Parent root = loader.load();
 		Scene scene = new Scene(root, 500, 800);
+        logger.info("Loading Hangman.css");
 		scene.getStylesheets().add(getClass().getResource("Hangman.css").toExternalForm());
 		primaryStage.setScene(scene);
+        logger.info("Presenting Application");
 		primaryStage.show();
 	}
 
